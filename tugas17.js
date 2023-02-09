8b43ef5b15e5c3281bada47706c7c57e77a14df9
dnriski <nugrohoriski159@gmail.com>
2023-02-09 18:39:11 +0700

    first commit

---
+++ tugas17.js
@@ -0,0 +1,19 @@
+var saya = "Saya beLajar bahaSa peMrograman deNgan khUsuk.";
+console.log(saya.toLowerCase());
+console.log(saya.toUpperCase());
+
+var kata =
+  "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+console.log(kata.toLowerCase());
+console.log(kata.toUpperCase());
+
+var lorem =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+var satu1 = lorem.charCodeAt(0);
+var satu2 = lorem.charCodeAt(1);
+var satu3 = lorem.charCodeAt(2);
+var satu4 = lorem.charCodeAt(3);
+var satu5 = lorem.charCodeAt(4);
+var satu6 = lorem.charCodeAt(5);
+
+console.log(satu1, "-", satu2, "-", satu3, "-", satu4, "-", satu5, "-", satu6);
